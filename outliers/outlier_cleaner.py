32f23342d294a64f314bb2b57bcf71e4ca855d33
pawel <pawel.fran@gmail.com>
2017-06-15 15:04:38 +0200

    exercises from outliers section

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,11 +10,24 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
+    import numpy as np
+    import pandas as pd
     
     cleaned_data = []
 
     ### your code goes here
+    array = np.concatenate((ages, net_worths, predictions), axis = 1) 
+    
+    df = pd.DataFrame(array, columns = ['ages', 'net_worths', 'predictions'])
+    df['prediction_error'] = (df['net_worths'] - df['predictions']).abs()
+    df_sorted = df.sort_values('prediction_error')
+    df_truncated = df_sorted.iloc[0:81] # take first 81 rows out of 90
 
+    for row in df_truncated.iterrows():
+	data = row[1]
+    	cleaned_data.append((data[0], data[1], data[2]))
+
+#    cleaned_data = []
     
     return cleaned_data
 
